17c6f31e692d7faed694d30a1daf8c1d4838f39d
jmosh6 <JoshRY.Mosh@gmail.com>
2024-12-02 14:12:36 -0500

    Add project files.

---
+++ ExampleProject/App.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject
+{
+    public class App : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddButtons(application);
+            return Result.Succeeded;
+        }
+
+        public void AddButtons(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            String tabName = "Example";
+            try
+            {
+                application.CreateRibbonTab(tabName);
+            }
+            catch (Autodesk.Revit.Exceptions.ArgumentException)
+            {
+            }
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Test");
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            // create push button for Assign Run Id
+            PushButtonData testData = new PushButtonData(
+                "Test",
+                "Test",
+                thisAssemblyPath,
+                "ExampleProject.TestCommand");
+
+            PushButton testB = ribbonPanel.AddItem(testData) as PushButton;
+            testB.ToolTip = "";
+        }
+    }
+}
---
+++ ExampleProject/TestCommand.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject
+{
+    [Transaction(TransactionMode.Manual)]
+    public class TestCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                throw new Exception("This is a test error");
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error",ex.Message);
+                TaskDialog.Show("Error",ex.StackTrace);
+            }
+            return Result.Succeeded;
+        }
+    }
+}
